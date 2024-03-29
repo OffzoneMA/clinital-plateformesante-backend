e657d8de87f902723850a739eaab9605459c8b07
Jamila <Jamila@gmail.com>
2024-03-29 18:33:58 +0000

    modification in DocumentResponse file

--- src/main/java/com/clinitalPlatform/payload/response/DocumentResponse.java
+++ src/main/java/com/clinitalPlatform/payload/response/DocumentResponse.java
@@ -2,6 +2,8 @@ package com.clinitalPlatform.payload.response;
 
 import com.clinitalPlatform.models.Patient;
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.Date;
 
 import java.util.Date;
 
@@ -11,7 +13,10 @@ public class DocumentResponse {
 	private Long id_doc;
 	private Long numero_doc;
 	private String titre_doc;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC")
 	private Date date_ajout_doc;
+
 	private String auteur;
 	private String fichier_doc;
 	private Patient patient;
