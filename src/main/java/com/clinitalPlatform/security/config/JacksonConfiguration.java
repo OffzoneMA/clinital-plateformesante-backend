d384050f3c6b410ed4b28694c4ed0bd6e5bf9b8c
Regoug Zakia <regougzakia@gmail.com>
2024-04-02 16:04:50 +0000

    conf merge

--- src/main/java/com/clinitalPlatform/security/config/JacksonConfiguration.java
+++ src/main/java/com/clinitalPlatform/security/config/JacksonConfiguration.java
@@ -1,17 +1,20 @@
-package com.clinitalPlatform.security.config;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class JacksonConfiguration {
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-        return objectMapper;
-    }
-}
+//package com.clinitalPlatform.security.config;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.SerializationFeature;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class JacksonConfiguration {
+//
+//    @Bean
+//    public ObjectMapper objectMapper() {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+//        return objectMapper;
+////        ObjectMapper mapper = new ObjectMapper();
+////        mapper.registerModule(new JavaTimeModule());
+////        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+//    }
+//}
